eb4d26372de7a588fb7995335909f474e172f21f
TEQ <thomazkwon@gmail.com>
2023-06-12 16:10:54 +0900

    fix one name add palindrome

---
+++ Easy/PalindromeNumber.java
@@ -0,0 +1,18 @@
+class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        } else if (x == 0) {
+            return true;
+        } else {
+            String num = Integer.toString(x);
+            StringBuffer sb = new StringBuffer(num);
+            String reversedNum = sb.reverse().toString();
+            if (num.equals(reversedNum)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
