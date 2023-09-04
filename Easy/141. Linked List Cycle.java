7c74794cf6741b0e9ff88d683e2a2b37b6b8b863
teq <thomazkwon@gmail.com>
2023-09-04 11:33:34 +0900

    fix linkedlist cycle to two pointer

--- Easy/141. Linked List Cycle.java
+++ Easy/141. Linked List Cycle.java
@@ -22,3 +22,37 @@ public class Solution {
         return false;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution {
+    // public boolean hasCycle(ListNode head) {
+    // Map<ListNode, Integer> hm = new HashMap<>();
+    // while(head != null) {
+    // if (hm.containsKey(head)) return true;
+    // else hm.put(head, 1);
+    // head = head.next;
+    // }
+    // return false;
+    // }
+    public boolean hasCycle(ListNode head) {
+        ListNode slowPointer = head, fastPointer = head;
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+            if (slowPointer == fastPointer) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
