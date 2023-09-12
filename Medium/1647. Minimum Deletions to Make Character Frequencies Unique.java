571f94b8baa12bec500e7a70472684edd62c0ed8
teq <thomazkwon@gmail.com>
2023-09-12 13:08:37 +0900

    fix 1647 from hm to array

--- Medium/1647. Minimum Deletions to Make Character Frequencies Unique.java
+++ Medium/1647. Minimum Deletions to Make Character Frequencies Unique.java
@@ -1,12 +1,17 @@
 class Solution {
     public int minDeletions(String s) {
-        Map<Character, Integer> hm = new HashMap<>();
+        // Map<Character, Integer> hm = new HashMap<>();
+        // for (char c : s.toCharArray()) {
+        // hm.put(c, hm.getOrDefault(c, 0) + 1);
+        // }
+        int[] freqs = new int[27];
         for (char c : s.toCharArray()) {
-            hm.put(c, hm.getOrDefault(c, 0) + 1);
+            freqs[c - 'a']++;
         }
         Set<Integer> hs = new HashSet<>();
         int cnt = 0;
-        for (int freq : hm.values()) {
+        // for (int freq : hm.values()) {
+        for (int freq : freqs) {
             while (freq > 0 && hs.contains(freq)) {
                 freq--;
                 cnt++;
