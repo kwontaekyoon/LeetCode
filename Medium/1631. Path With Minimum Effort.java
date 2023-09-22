ffab7f10698a816d0c180eade0583aac4bdd2d63
teq <thomazkwon@gmail.com>
2023-09-22 16:37:59 +0900

    fix 1631 from integer arrays node to class Node

--- Medium/1631. Path With Minimum Effort.java
+++ Medium/1631. Path With Minimum Effort.java
@@ -1,70 +1,44 @@
-// backtracking -> time limit exceeded
-// class Solution {
-//     private int minEffort = Integer.MAX_VALUE, R = 0, C = 0;
-//     private int[][] dirs = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-
-//     private void backtrack(int cr, int cc, int currEffort, int[][] heights, int[][] dists) {
-//         if (cr == R - 1 && cc == C - 1) {
-//             minEffort = currEffort;
-//         } else {
-//             for (int[] dir : dirs) {
-//                 int nr = cr + dir[0], nc = cc + dir[1];
-//                 if (nr >= 0 && nc >= 0 && nr < R && nc < C) {
-//                     int newEffort = Math.max(Math.abs(heights[nr][nc] - heights[cr][cc]), currEffort);
-//                     if (newEffort < dists[nr][nc]) {
-//                         dists[nr][nc] = newEffort;
-//                         backtrack(nr, nc, newEffort, heights, dists);
-//                     }
-//                 }
-//             }
-//         }
-//     }
-//     public int minimumEffortPath(int[][] heights) {
-//         R = heights.length;
-//         C = heights[0].length;
-//         int[][] dists = new int[R][C];
-//         for (int[] dist : dists) {
-//             Arrays.fill(dist, Integer.MAX_VALUE);
-//         }
-//         dists[0][0] = 0;
-//         backtrack(0, 0, 0, heights, dists);
-//         return minEffort;
-//     }
-// }
-
 // dijkstra -> shortest path from a graph of weighted nodes
 public class Solution {
+    private class Node {
+        private int r;
+        private int c;
+        private int effort;
+
+        private Node(int r, int c, int effort) {
+            this.r = r;
+            this.c = c;
+            this.effort = effort;
+        }
+    }
+
     public int minimumEffortPath(int[][] heights) {
         int rows = heights.length, cols = heights[0].length;
         int[][] dist = new int[rows][cols];
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
-        minHeap.add(new int[] { 0, 0, 0 });
-
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 dist[i][j] = Integer.MAX_VALUE;
             }
         }
         dist[0][0] = 0;
-
         int[][] dirs = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-
+        PriorityQueue<Node> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a.effort, b.effort));
+        minHeap.offer(new Node(0, 0, 0));
         while (!minHeap.isEmpty()) {
-            int[] top = minHeap.poll();
-            int effort = top[0], x = top[1], y = top[2];
-
-            if (x == rows - 1 && y == cols - 1) {
+            Node top = minHeap.poll();
+            int effort = top.effort, r = top.r, c = top.c;
+            if (r == rows - 1 && c == cols - 1) {
                 return effort;
             }
-
             for (int[] dir : dirs) {
-                int nx = x + dir[0], ny = y + dir[1];
-                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols) {
-                    int new_effort = Math.max(effort, Math.abs(heights[x][y] - heights[nx][ny]));
-                    if (new_effort < dist[nx][ny]) {
-                        dist[nx][ny] = new_effort;
-                        minHeap.add(new int[] { new_effort, nx, ny });
-                    }
+                int nr = r + dir[0], nc = c + dir[1];
+                if (nr < 0 || nr >= rows || nc < 0 || nc >= cols) {
+                    continue;
+                }
+                int newEffort = Math.max(effort, Math.abs(heights[r][c] - heights[nr][nc]));
+                if (newEffort < dist[nr][nc]) {
+                    dist[nr][nc] = newEffort;
+                    minHeap.offer(new Node(nr, nc, newEffort));
                 }
             }
         }
