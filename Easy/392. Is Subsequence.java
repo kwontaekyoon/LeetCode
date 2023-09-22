cc001c0b4d3b7b678cf5fc71336a147729c9c858
teq <thomazkwon@gmail.com>
2023-09-22 09:22:21 +0900

    fix 392 to generic way

--- Easy/392. Is Subsequence.java
+++ Easy/392. Is Subsequence.java
@@ -1,13 +1,21 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        if (s.isBlank())
-            return true;
-        int p = 0;
-        for (char c : t.toCharArray()) {
-            if (c == s.charAt(p))
-                if (++p == s.length())
-                    return true;
+        // if (s.isBlank())
+        // return true;
+        // int p = 0;
+        // for (char c : t.toCharArray()) {
+        // if (c == s.charAt(p))
+        // if (++p == s.length())
+        // return true;
+        // }
+        // return false;
+
+        int pt1 = 0;
+        for (int pt2 = 0; pt1 < s.length() && pt2 < t.length(); pt2++) {
+            if (s.charAt(pt1) == t.charAt(pt2)) {
+                pt1 += 1;
+            }
         }
-        return false;
+        return pt1 == s.length() ? true : false;
     }
 }
