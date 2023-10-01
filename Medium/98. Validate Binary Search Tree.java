8eba3c701fb3ceb041e62717fb0fbd8c7f906636
teq <thomazkwon@gmail.com>
2023-10-01 20:20:22 +0900

    add medium welldone recursion bst

---
+++ Medium/98. Validate Binary Search Tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    private boolean checkBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val >= maxVal || root.val <= minVal) {
+            return false;
+        }
+        return checkBST(root.left, minVal, root.val) && checkBST(root.right, root.val, maxVal);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return checkBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+}
