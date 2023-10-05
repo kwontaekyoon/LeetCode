dbba1932d86f6b3547ee1ed793b9c85fb505d402
teq <thomazkwon@gmail.com>
2023-10-05 18:40:14 +0900

    fix 2610 get -> getOrDefault

--- Medium/2610. Convert an Array Into a 2D Array With Conditions.java
+++ Medium/2610. Convert an Array Into a 2D Array With Conditions.java
@@ -3,20 +3,12 @@ class Solution {
         Map<Integer, Integer> hm = new HashMap<>();
         List<List<Integer>> matrix = new ArrayList<>();
         for (int num : nums) {
-            if (hm.containsKey(num)) {
-                int idx = hm.get(num);
-                hm.put(num, idx + 1);
-                if (matrix.size() <= idx) {
-                    matrix.add(new ArrayList<>());
-                }
-                matrix.get(idx).add(num);
-            } else {
-                hm.put(num, 1);
-                if (matrix.size() == 0) {
-                    matrix.add(new ArrayList<>());
-                }
-                matrix.get(0).add(num);
+            int idx = hm.getOrDefault(num, 0);
+            hm.put(num, idx + 1);
+            if (matrix.size() < idx + 1) {
+                matrix.add(new ArrayList<>());
             }
+            matrix.get(idx).add(num);
         }
         return matrix;
     }
