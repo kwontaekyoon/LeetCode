bf941bfbea2ac9d64a3b1011fe323badf4119e82
teq <thomazkwon@gmail.com>
2023-10-10 16:50:53 +0900

    fix 2009 optimization

--- Hard/2009. Minimum Number of Operations to Make Array Continuous.java
+++ Hard/2009. Minimum Number of Operations to Make Array Continuous.java
@@ -1,25 +1,49 @@
 class Solution {
     public int minOperations(int[] nums) {
-        Arrays.sort(nums); // Sort the array
-
+        Arrays.sort(nums);
         int n = nums.length;
-        int maxNumsInWindow = 0;
-
-        Deque<Integer> numsInWindow = new ArrayDeque<>();
-        for (int num : nums) {
-            // Advance the window
-            while (numsInWindow.size() > 0 && num - numsInWindow.peekFirst() >= n) {
-                numsInWindow.pollFirst();
+        int u = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[u++] = nums[i];
             }
-
-            // Add the new number to the window (if it's not a duplicate)
-            if (numsInWindow.size() == 0 || !numsInWindow.peekLast().equals(num)) {
-                numsInWindow.offerLast(num);
-            }
-
-            maxNumsInWindow = Math.max(maxNumsInWindow, numsInWindow.size());
         }
-
-        return n - maxNumsInWindow;
+        int ans = nums.length;
+        for (int i = 0, j = 0; i < u; i++) {
+            while (j < u && nums[j] - nums[i] <= n - 1) {
+                j++;
+            }
+            ans = Math.min(ans, n - j + i);
+        }
+        return ans;
     }
 }
+
+/**
+ * sliding window using deque
+ * class Solution {
+ * public int minOperations(int[] nums) {
+ * Arrays.sort(nums); // Sort the array
+ * 
+ * int n = nums.length;
+ * int maxNumsInWindow = 0;
+ * 
+ * Deque<Integer> numsInWindow = new ArrayDeque<>();
+ * for (int num : nums) {
+ * // Advance the window
+ * while (numsInWindow.size() > 0 && num - numsInWindow.peekFirst() >= n) {
+ * numsInWindow.poll();
+ * }
+ * 
+ * // Add the new number to the window (if it's not a duplicate)
+ * if(numsInWindow.size() == 0 || !numsInWindow.peekLast().equals(num)) {
+ * numsInWindow.offer(num);
+ * }
+ * 
+ * maxNumsInWindow = Math.max(maxNumsInWindow, numsInWindow.size());
+ * }
+ * 
+ * return n - maxNumsInWindow;
+ * }
+ * }
+ */
