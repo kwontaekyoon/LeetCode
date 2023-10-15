1adecb35ccfec7dbb8a7834b5a7e9edd6da2323f
teq <thomazkwon@gmail.com>
2023-10-15 19:10:07 +0900

    add memoziation for 2742

--- Hard/2742. Painting the Walls.java
+++ Hard/2742. Painting the Walls.java
@@ -1,4 +1,4 @@
-class Solution {
+class tabulation {
     public int paintWalls(int[] cost, int[] time) {
         int n = cost.length, dp[] = new int[n + 1];
         Arrays.fill(dp, (int) 1e9);
@@ -9,5 +9,28 @@ class Solution {
             }
         }
         return dp[n];
+    }
+}
+
+class memo {
+    private int dfs(int[] cost, int[] time, int index, int remain, int[][] dp) {
+        if (remain <= 0)
+            return 0;
+        if (index >= cost.length)
+            return (int) 1e9;
+        if (dp[index][remain] != -1)
+            return dp[index][remain];
+        int take = cost[index] + dfs(cost, time, index + 1, remain - 1 - time[index], dp);
+        int notTake = dfs(cost, time, index + 1, remain, dp);
+        return dp[index][remain] = Math.min(take, notTake);
+    }
+
+    public int paintWalls(int[] cost, int[] time) {
+        int n = cost.length;
+        int[][] dp = new int[n + 1][n + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return dfs(cost, time, 0, n, dp);
     }
 }
