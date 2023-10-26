943d53ecd4e2c6d0ce637ab8493c14c4c918be26
TEQ <45059797+thomazkwon@users.noreply.github.com>
2023-10-26 17:54:52 +0900

    Update 823. Binary Trees With Factors.java

--- Medium/823. Binary Trees With Factors.java
+++ Medium/823. Binary Trees With Factors.java
@@ -2,24 +2,22 @@ class Solution {
     private static final int MOD = (int) 1e9 + 7;
     public int numFactoredBinaryTrees(int[] arr) {
         Arrays.sort(arr);
-        Set<Integer> hs = new HashSet<>();
         Map<Integer, Integer> dp = new HashMap<>();
         for(int num : arr) {
-            hs.add(num);
             dp.put(num, 1);
         }
-        for (int i : arr) {
-            for (int j : arr) {
-                if (j > Math.sqrt(i)) {
+        for (int root : arr) {
+            int upperBound = (int) Math.sqrt(root);
+            for (int mod : arr) {
+                if (mod > upperBound) {
                     break;
                 }
-                if (i % j == 0 && hs.contains(i / j)) {
-                    long tmp = (long) dp.get(j) * dp.get(i / j);
-                    dp.put(i, (int) ((dp.get(i) + (i / j == j ? tmp : tmp * 2)) % MOD));
+                if (root % mod == 0 && dp.containsKey(root / mod)) {
+                    long tmp = (long) dp.get(mod) * dp.get(root / mod);
+                    dp.put(root, (int) ((dp.get(root) + (root / mod == mod ? tmp : tmp * 2)) % MOD));
                 }
             }
         }
-
         int res = 0;
         for (int val : dp.values()) {
             res  = (res + val) % MOD;
