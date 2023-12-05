0ed876331ffa5bf7efaa8a6a5d73ee1242d3d427
teq <thomazkwon@gmail.com>
2023-12-05 15:06:48 +0900

    create medium hash table

---
+++ Medium/1930. Unique Length-3 Palindromic Subsequences.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        Set<Character> startAndEnd = new HashSet<>();
+        int numOfUniqueSeq = 0;
+        for (int l = 0; l < s.length(); l++) {
+            if (startAndEnd.contains(s.charAt(l))) {
+                continue;
+            }
+            startAndEnd.add(s.charAt(l));
+            for (int r = s.length() - 1; r >= l + 2; r--) {
+                if (s.charAt(r) != s.charAt(l)) {
+                    continue;
+                }
+                Set<Character> uniqueMids = new HashSet<>();
+                for (int m = r - 1; m > l && uniqueMids.size() < 26; m--) {
+                    uniqueMids.add(s.charAt(m));
+                }
+                numOfUniqueSeq += uniqueMids.size();
+                break;
+            }
+        }
+        return numOfUniqueSeq;
+    }
+}
