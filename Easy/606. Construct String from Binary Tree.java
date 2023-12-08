afd2778a21b04cf2fddc20e7c5541c2a43f11ce3
teq <thomazkwon@gmail.com>
2023-12-08 10:20:47 +0900

    create easy dfs, recursion

---
+++ Easy/606. Construct String from Binary Tree.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public String tree2str(TreeNode root) {
+        StringBuilder res = new StringBuilder();
+        dfs(root, res);
+        return res.toString();
+    }
+
+    private void dfs(TreeNode node, StringBuilder res) {
+        if (node == null) {
+            return;
+        }
+        res.append(node.val);
+        if (node.left == null && node.right != null) {
+            res.append("()");
+            res.append("(");
+            dfs(node.right, res);
+            res.append(")");
+        }
+        if (node.left != null && node.right == null) {
+            res.append("(");
+            dfs(node.left, res);
+            res.append(")");
+        }
+        if (node.left != null && node.right != null) {
+            res.append("(");
+            dfs(node.left, res);
+            res.append(")");
+            res.append("(");
+            dfs(node.right, res);
+            res.append(")");
+        }
+    }
+}
