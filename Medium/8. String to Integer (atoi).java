526d01d4b6c8be52e8feabde68c4c8ba97749a6e
teq <thomazkwon@gmail.com>
2023-12-16 16:33:57 +0900

    create medium rare string

---
+++ Medium/8. String to Integer (atoi).java
@@ -0,0 +1,30 @@
+class Solution {
+    public int myAtoi(String s) {
+        char[] sToChars = s.toCharArray();
+        int i = 0;
+        int n = s.length();
+        int p = 1;
+        long r = 0;
+        while (i < n && sToChars[i] == ' ') {
+            i += 1;
+        }
+        if (i == n) {
+            return 0;
+        }
+        if (sToChars[i] == '-') {
+            p = -1;
+            i += 1;
+        } else if (sToChars[i] == '+') {
+            p = 1;
+            i += 1;
+        }
+        while (i < n && sToChars[i] >= '0' && sToChars[i] <= '9') {
+            r = r * 10 + (sToChars[i] - '0');
+            i += 1;
+            if (r > Integer.MAX_VALUE) {
+                return p == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+        }
+        return (int) r * p;
+    }
+}
