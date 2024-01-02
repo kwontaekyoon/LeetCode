2a294096ab2c5804a397e6b2e20e14b43b231e3e
teq <thomazkwon@gmail.com>
2024-01-02 09:20:00 +0900

    update medium hash table

--- Medium/2610. Convert an Array Into a 2D Array With Conditions.java
+++ Medium/2610. Convert an Array Into a 2D Array With Conditions.java
@@ -1,4 +1,5 @@
 class Solution {
+    // map
     public List<List<Integer>> findMatrix(int[] nums) {
         Map<Integer, Integer> hm = new HashMap<>();
         List<List<Integer>> matrix = new ArrayList<>();
@@ -9,6 +10,22 @@ class Solution {
                 matrix.add(new ArrayList<>());
             }
             matrix.get(idx).add(num);
+        }
+        return matrix;
+    }
+
+    // counter
+    public List<List<Integer>> findMatrix(int[] nums) {
+        int n = nums.length;
+        int[] count = new int[n + 1];
+        List<List<Integer>> matrix = new ArrayList<>();
+        for (int num : nums) {
+            int cnt = count[num];
+            if (matrix.size() == cnt) {
+                matrix.add(new ArrayList<>());
+            }
+            matrix.get(cnt).add(num);
+            count[num]++;
         }
         return matrix;
     }
