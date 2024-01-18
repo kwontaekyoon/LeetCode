faf8adca480fb2eeba91b6a12f352d0c288b768d
kwontaekyoon <thomazkwon@gmail.com>
2024-01-18 09:28:47 +0900

    update easy dp memo

--- Easy/70. Climbing Stairs.java
+++ Easy/70. Climbing Stairs.java
@@ -1,3 +1,4 @@
+// tabulation
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
@@ -7,5 +8,22 @@ class Solution {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
+    }
+}
+
+// memoziation
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        return memo(n, dp);
+    }
+    private int memo(int step, int[] dp) {
+        if (step == 0 || step == 1) {
+            return 1;
+        }
+        if (dp[step] != 0) {
+            return dp[step];
+        }
+        return dp[step] = memo(step - 1, dp) + memo(step - 2, dp);
     }
 }
