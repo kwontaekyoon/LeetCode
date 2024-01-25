6ae171138e75c9464b36f1072f5a85ca50a05943
kwontaekyoon <thomazkwon@gmail.com>
2024-01-25 16:15:48 +0900

    update 1143 dp memory optimization

--- Medium/1143. Longest Common Subsequence.java
+++ Medium/1143. Longest Common Subsequence.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
+    public int 2D(String text1, String text2) {
         int len1 = text1.length(), len2 = text2.length();
         char[] text1ToChars = text1.toCharArray();
         char[] text2ToChars = text2.toCharArray();
@@ -16,5 +16,30 @@ class Solution {
         }
 
         return dp[len1][len2];
+    }
+
+    public int 1D(String text1, String text2) {
+        if (text1.length() > text2.length()) {
+            return helper(text1, text2);
+        } else {
+            return helper(text2, text1);
+        }
+    }
+    private int helper(String longer, String shorer) {
+        int longLen = longer.length(), shorLen = shorer.length();
+        char[] L = longer.toCharArray(), S = shorer.toCharArray();
+        int [] dp = new int[shorLen + 1];
+        for (int r = 0; r < longLen; r++) {
+            int[] tmp = new int[shorLen + 1];
+            for (int c = 0; c < shorLen; c++) {
+                if (L[r] == S[c]) {
+                    tmp[c + 1] = dp[c] + 1;
+                } else {
+                    tmp[c + 1] = Math.max(tmp[c], dp[c + 1]);
+                }
+            }
+            dp = tmp;
+        }
+        return dp[shorLen];
     }
 }
