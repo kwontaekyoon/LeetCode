15eab6483ad3fa05079ec4dc35d9fc6e6086cfaa
kwontaekyoon <thomazkwon@gmail.com>
2024-01-26 11:25:06 +0900

    update medium dp memoziation

--- Medium/576. Out of Boundary Paths.java
+++ Medium/576. Out of Boundary Paths.java
@@ -2,7 +2,8 @@ class Solution {
     private final static int MOD = (int) 1e9 + 7;
     private int[] dir = { -1, 0, 1, 0, -1 };
 
-    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+    // sol 1
+    public int tabulation(int m, int n, int maxMove, int startRow, int startColumn) {
         if (maxMove == 0) {
             return 0;
         }
@@ -31,5 +32,28 @@ class Solution {
 
     private boolean isValid(int R, int C, int r, int c) {
         return r > -1 && c > -1 && r < R && c < C;
+    }
+
+    // sol 2
+    public int memoziation(int m, int n, int maxMove, int startRow, int startColumn) {
+        return memo(m, n, maxMove, startRow, startColumn, new Integer[m][n][maxMove + 1]);
+    }
+
+    private int memo(int R, int C, int move, int r, int c, Integer[][][] dp) {
+        if (r < 0 || c < 0 || r == R || c == C) {
+            return 1;
+        }
+        if (move < 1) {
+            return 0;
+        }
+        if (dp[r][c][move] != null) {
+            return dp[r][c][move];
+        }
+        int paths = 0;
+        paths = (paths + memo(R, C, move - 1, r - 1, c, dp)) % MOD;
+        paths = (paths + memo(R, C, move - 1, r + 1, c, dp)) % MOD;
+        paths = (paths + memo(R, C, move - 1, r, c - 1, dp)) % MOD;
+        paths = (paths + memo(R, C, move - 1, r, c + 1, dp)) % MOD;
+        return dp[r][c][move] = paths;
     }
 }
