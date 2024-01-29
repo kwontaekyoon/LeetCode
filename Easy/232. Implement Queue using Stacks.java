72e83fc896be219effedbe20fec88a55f3768455
kwontaekyoon <thomazkwon@gmail.com>
2024-01-29 09:36:36 +0900

    create easy implementation stack queue

---
+++ Easy/232. Implement Queue using Stacks.java
@@ -0,0 +1,40 @@
+class MyQueue {
+    private Deque<Integer> stack;
+    private Deque<Integer> tmp;
+
+    public MyQueue() {
+        stack = new ArrayDeque<>();
+    }
+    
+    public void push(int x) {
+        tmp = new ArrayDeque<>();
+        while (!stack.isEmpty()) {
+            tmp.push(stack.pop());
+        }
+        stack.push(x);
+        while (!tmp.isEmpty()) {
+            stack.push(tmp.pop());
+        }
+    }
+    
+    public int pop() {
+        return stack.pop();
+    }
+    
+    public int peek() {
+        return stack.peek();
+    }
+    
+    public boolean empty() {
+        return stack.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
