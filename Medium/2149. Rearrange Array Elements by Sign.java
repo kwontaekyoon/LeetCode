b939f16c72c9a1c143ded15aca3c0db6163a9f76
kwontaekyoon <thomazkwon@gmail.com>
2024-02-16 17:52:51 +0900

    create medium array

---
+++ Medium/2149. Rearrange Array Elements by Sign.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int pos = 0, neg = 1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
+                res[neg] = nums[i];
+                neg += 2;
+            } else {
+                res[pos] = nums[i];
+                pos += 2;
+            }
+        }
+        return res;
+    }
+}
