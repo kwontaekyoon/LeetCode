ec7b4576ffa68c23c834ac1d85519e1b85ee76b5
kwontaekyoon <thomazkwon@gmail.com>
2024-02-19 09:19:09 +0900

    update medium heap

--- Medium/1642. Furthest Building You Can Reach.java
+++ Medium/1642. Furthest Building You Can Reach.java
@@ -1,26 +1,26 @@
 class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
         PriorityQueue<Integer> usedLadders = new PriorityQueue<>();
-        int usedBricks = 0;
-        for (int i = 1; i < heights.length; i++) {
+        int usedBricks = 0, n = heights.length;
+        for (int i = 1; i < n; i++) {
             int diff = heights[i] - heights[i - 1];
             if (diff <= 0) {
                 continue;
             }
-            if (usedLadders.size() == ladders) {
+            if (usedLadders.size() < ladders) {
+                usedLadders.offer(diff);
+            } else {
                 if (ladders != 0 && usedLadders.peek() < diff) {
                     usedBricks += usedLadders.poll();
                     usedLadders.offer(diff);
                 } else {
                     usedBricks += diff;
                 }
-            } else {
-                usedLadders.offer(diff);
-            }
-            if (usedLadders.size() == ladders && usedBricks > bricks) {
-                return i - 1;
+                if (usedBricks > bricks) {
+                    return i - 1;
+                }
             }
         }
-        return heights.length - 1;
+        return n - 1;
     }
 }
