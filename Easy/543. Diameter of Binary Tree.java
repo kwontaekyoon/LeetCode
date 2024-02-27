a8d3beb06e05969c6a0be8fcca7c2de38d4383d1
kwontaekyoon <thomazkwon@gmail.com>
2024-02-27 09:37:40 +0900

    update easy dfs

--- Easy/543. Diameter of Binary Tree.java
+++ Easy/543. Diameter of Binary Tree.java
@@ -1,15 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     private int maxDiameter = 0;
     public int diameterOfBinaryTree(TreeNode root) {
-        findDepth(root);
+        dfs(root);
         return maxDiameter;
     }
-    private int findDepth(TreeNode node) {
+    private int dfs(TreeNode node) {
         if (node == null) {
             return 0;
         }
-        int left = findDepth(node.left);
-        int right = findDepth(node.right);
+        int left = dfs(node.left);
+        int right = dfs(node.right);
         maxDiameter = Math.max(maxDiameter, left + right);
         return Math.max(left, right) + 1;
     }
