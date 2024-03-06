be489ea1d28a41e8cfc81e477bc8ad014f103f47
kwontaekyoon <thomazkwon@gmail.com>
2024-03-06 09:21:27 +0000

    update easy two pointers

--- Easy/141. Linked List Cycle.java
+++ Easy/141. Linked List Cycle.java
@@ -1,14 +1,3 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) {
- * val = x;
- * next = null;
- * }
- * }
- */
 public class Solution {
     public boolean hasCycle(ListNode head) {
         Map<ListNode, Integer> hm = new HashMap<>();
@@ -21,35 +10,17 @@ public class Solution {
         }
         return false;
     }
+    
 }
 
-/**
- * Definition for singly-linked list.
- * class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) {
- * val = x;
- * next = null;
- * }
- * }
- */
-public class Solution {
-    // public boolean hasCycle(ListNode head) {
-    // Map<ListNode, Integer> hm = new HashMap<>();
-    // while(head != null) {
-    // if (hm.containsKey(head)) return true;
-    // else hm.put(head, 1);
-    // head = head.next;
-    // }
-    // return false;
-    // }
+public class TwoPointers {
     public boolean hasCycle(ListNode head) {
-        ListNode slowPointer = head, fastPointer = head;
-        while (fastPointer != null && fastPointer.next != null) {
-            slowPointer = slowPointer.next;
-            fastPointer = fastPointer.next.next;
-            if (slowPointer == fastPointer) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
                 return true;
             }
         }
