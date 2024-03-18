dcd9ddddec04f11a880c01e8c582ed54431e1a53
kwontaekyoon <thomazkwon@gmail.com>
2024-03-18 17:44:07 +0000

    update medium sorting greedy

--- Medium/435. Non-overlapping Intervals.java
+++ Medium/435. Non-overlapping Intervals.java
@@ -1,12 +1,29 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (x1, x2) -> Integer.compare(x1[1], x2[1]));
-        int prev = intervals[0];
-        int cnt = intervals.length - 1;
-        for (int[] interval : intervals) {
-            if (interval[0] >= prev[1]) {
-                cnt--;
-                prev = interval;
+        Arrays.sort(intervals, (x, y) -> Integer.compare(x[1], y[1]));
+        int[] prev = { Integer.MIN_VALUE, Integer.MIN_VALUE };
+        int cnt = 0;
+        for (int[] curr : intervals) {
+            if (curr[0] >= prev[1]) {
+                cnt++;
+                prev = curr;
+            }
+        }
+        return intervals.length - cnt;
+    }
+}
+
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (x, y) -> Integer.compare(x[0], y[0]));
+        int cnt = 0;
+        int end = Integer.MIN_VALUE;
+        for (int[] cur : intervals) {
+            if (cur[0] >= end) {
+                end = cur[1];
+            } else {
+                end = Math.min(cur[1], end);
+                cnt++;
             }
         }
         return cnt;
