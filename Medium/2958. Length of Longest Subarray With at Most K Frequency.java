3e4027f3005a7dc03406dfb8d260e01d40b4f492
kwontaekyoon <thomazkwon@gmail.com>
2024-03-28 09:54:49 +0000

    update medium sliding window hash table queue

--- Medium/2958. Length of Longest Subarray With at Most K Frequency.java
+++ Medium/2958. Length of Longest Subarray With at Most K Frequency.java
@@ -1,4 +1,22 @@
-class Solution {
+class Modified {
+    public int maxSubarrayLength(int[] nums, int k) {
+        var map = new HashMap<Integer, ArrayDeque<Integer>>();
+        int ans = 0;
+        for (int l = 0, r = 0; r < nums.length; r++) {
+            map.computeIfAbsent(nums[r], key -> new ArrayDeque<>()).offer(r);
+            if (map.get(nums[r]).size() > k) {
+                if (map.get(nums[r]).peekFirst() >= l) {
+                    l = map.get(nums[r]).peekFirst() + 1;
+                }
+                map.get(nums[r]).poll();
+            }
+            ans = Math.max(ans, r - l + 1);
+        }
+        return ans;
+    }
+}
+
+class Naive {
     public int maxSubarrayLength(int[] nums, int k) {
         var map = new HashMap<Integer, Integer>();
         int res = 0;
