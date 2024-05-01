bc48091576502fb423b11c9e171f009a966f696c
kwontaekyoon <thomazkwon@gmail.com>
2024-05-01 10:17:06 +0100

    update easy string array

--- Easy/2000. Reverse Prefix of Word.java
+++ Easy/2000. Reverse Prefix of Word.java
@@ -12,3 +12,18 @@ class Solution {
         return sb.toString();
     }
 }
+
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int idx = word.indexOf(ch);
+        if (idx == -1) {
+            return word;
+        }
+
+        return new StringBuilder()
+                .append(word.substring(0, idx + 1))
+                .reverse()
+                .append(word.substring(idx + 1, word.length()))
+                .toString();
+    }
+}
