2863c95eccd9a266555de3d7a083ebea05d981f1
kwontaekyoon <thomazkwon@gmail.com>
2024-05-05 14:18:34 +0100

    update easy linked list two pointers

--- Easy/203. Remove Linked List Elements.java
+++ Easy/203. Remove Linked List Elements.java
@@ -1,15 +1,33 @@
-class Solution {
-    public ListNode removeElements(ListNode head, int val) {
-        var dummy = new ListNode(-1, head);
-        var newHead = dummy;
-        while (head != null) {
-            if (head.val != val) {
-                newHead.next = head;
-                newHead = newHead.next;
-            }
-            head = head.next;
-            newHead.next = null;
+class UsingIterator {
+    public ListNode deleteMiddle(ListNode head) {
+        int size = 0;
+        var newHead = head;
+        while (newHead != null) {
+            size++;
+            newHead = newHead.next;
         }
+        var dummy = new ListNode(0, head);
+        head = dummy;
+        for (int i = 0; i < size / 2; i++) {
+            head = head.next;
+        }
+        head.next = head.next.next;
         return dummy.next;
+    }
+}
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null) {
+            return null;
+        }
+        var slow = head;
+        var fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return head;
     }
 }
