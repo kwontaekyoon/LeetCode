ec5c5daae3ee3ffab7deb2cf56da12681771498b
kwontaekyoon <thomazkwon@gmail.com>
2024-05-16 10:08:33 +0100

    create easy recursion

---
+++ Easy/2331. Evaluate Boolean Binary Tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        switch (root.val) {
+            default:
+                return root.val == 1;
+            case 2:
+                return evaluateTree(root.left) | evaluateTree(root.right);
+            case 3:
+                return evaluateTree(root.left) & evaluateTree(root.right);
+        }
+    }
+}
