99842ada5b37b015f0556448ecc16cb40b06c53e
kwontaekyoon <thomazkwon@gmail.com>
2024-05-17 13:10:09 +0100

    update medium dfs recursion

--- Medium/1325. Delete Leaves With a Given Value.java
+++ Medium/1325. Delete Leaves With a Given Value.java
@@ -1,25 +1,25 @@
 class Solution {
     public TreeNode removeLeafNodes(TreeNode root, int target) {
         var dummy = new TreeNode(0, null, root);
-        dfs(dummy, root, target, true);
+        dfs(dummy, root, target);
         return dummy.right;
     }
 
-    private void dfs(TreeNode prev, TreeNode curr, int target, boolean right) {
+    private void dfs(TreeNode prev, TreeNode curr, int target) {
         if (curr == null) {
             return;
         }
-        dfs(curr, curr.left, target, false);
-        dfs(curr, curr.right, target, true);
+        dfs(curr, curr.left, target);
+        dfs(curr, curr.right, target);
         if (curr.left == null && curr.right == null && curr.val == target) {
-            if (right) {
-                prev.right = null;
-            } else {
+            if (curr.equals(prev.left)) {
                 prev.left = null;
+            } else {
+                prev.right = null;
             }
         }
     }
-}
+}}
 
 class Solution1 {
     public TreeNode removeLeafNodes(TreeNode root, int target) {
