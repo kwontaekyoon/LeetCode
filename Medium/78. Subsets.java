82f0d272ad61dcb4c268a088459433fcebb32376
kwontaekyoon <thomazkwon@gmail.com>
2024-05-21 08:10:10 +0100

    update medium dfs backtracking iteration

--- Medium/78. Subsets.java
+++ Medium/78. Subsets.java
@@ -14,3 +14,40 @@ class Solution {
         return subsets;
     }
 }
+
+class Solution1 {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> curr = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(nums, 0);
+        return res;
+    }
+
+    private void dfs(int[] nums, int index) {
+        if (index == nums.length) {
+            res.add(new ArrayList(curr));
+            return;
+        }
+        dfs(nums, index + 1);
+        curr.add(nums[index]);
+        dfs(nums, index + 1);
+        curr.removeLast();
+    }
+}
+
+class Iteration {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>()); // Add the empty subset
+        for (int num : nums) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(result.get(i));
+                subset.add(num);
+                result.add(subset);
+            }
+        }
+        return result;
+    }
+}
