ac31d14ddbf86b3f01809a9b564c16f5e90bb4a5
kwontaekyoon <thomazkwon@gmail.com>
2024-06-12 10:10:48 +0100

    create medium sorting two pointers

--- Medium/75. Sort Colors.java
+++ Medium/75. Sort Colors.java
@@ -1,4 +1,25 @@
-class Solution {
+class OnePass {
+    public void sortColors(int[] nums) {
+        final int N = nums.length;
+        int two = N - 1, zero = 0;
+        for (int i = 0; i < N; i++) {
+            while (nums[i] == 2 && i < two) {
+                swap(nums, i, two--);
+            }
+            while (nums[i] == 0 && i > zero) {
+                swap(nums, i, zero++);
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
+
+class TwoPass {
     public void sortColors(int[] nums) {
         int[] freq = new int[3];
         for (int num : nums) {
