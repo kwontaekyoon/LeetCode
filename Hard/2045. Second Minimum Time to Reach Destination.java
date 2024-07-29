9cc6ac01d42fd75ee071504352c137babfafb02e
kwontaekyoon <thomazkwon@gmail.com>
2024-07-29 09:17:30 +0100

    update hard bfs

--- Hard/2045. Second Minimum Time to Reach Destination.java
+++ Hard/2045. Second Minimum Time to Reach Destination.java
@@ -1,29 +1,28 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
     public int secondMinimum(int n, int[][] edges, int time, int change) {
-        List<Integer>[] graph = new ArrayList[n + 1];
-        for (int i = 0; i < n + 1; i++)
-            graph[i] = new ArrayList<>();
-        for (int[] edge : edges) {
-            graph[edge[0]].add(edge[1]);
-            graph[edge[1]].add(edge[0]);
+        final var graph = new ArrayList<List<Integer>>();
+        for (int i = 0; i < n + 1; i++) {
+            graph.add(new ArrayList<>());
         }
-
-        Deque<int[]> deque = new LinkedList<>();
-        int[] visitedNum = new int[n + 1];
-        int[] timeArr = new int[n + 1];
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        final var deque = new ArrayDeque<int[]>();
+        final var visitedNum = new int[n + 1];
+        final var timeArr = new int[n + 1];
         Arrays.fill(timeArr, -1);
         deque.offerLast(new int[] { 1, 0 });
         timeArr[0] = 0;
 
         while (deque.size() > 0) {
             for (int i = deque.size(); i > 0; i--) {
-                int[] cur = deque.pollFirst();
+                var cur = deque.pollFirst();
                 int nextTime = 0;
                 int curLight = cur[1] / change;
                 if (curLight % 2 == 0) {
@@ -31,7 +30,7 @@ class Solution {
                 } else {
                     nextTime = (curLight + 1) * change + time;
                 }
-                for (int nextNode : graph[cur[0]]) {
+                for (int nextNode : graph.get(cur[0])) {
                     if (visitedNum[nextNode] < 2 && timeArr[nextNode] < nextTime) {
                         deque.offerLast(new int[] { nextNode, nextTime });
                         visitedNum[nextNode]++;
