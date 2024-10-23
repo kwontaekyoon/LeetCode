085201afd9aca16db741b8210e1c0772cecacb3e
kwontaekyoon <thomazkwon@gmail.com>
2024-10-23 16:02:32 +0100

    update medium bfs

--- Medium/2641. Cousins in Binary Tree II.py
+++ Medium/2641. Cousins in Binary Tree II.py
@@ -8,29 +8,24 @@ class Bfs:
     def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return root
-
         queue = deque([root])
-        curr_sum = root.val
-
+        curr_level_sum = root.val
         while queue:
-            size = len(queue)
-            next_sum = 0
-
-            for _ in range(size):
-                curr = queue.popleft()
-                curr.val = curr_sum - curr.val
-                sibling_sum = (curr.left.val if curr.left else 0) + (curr.right.val if curr.right else 0)
-                if curr.left:
-                    next_sum += curr.left.val
-                    curr.left.val = sibling_sum
-                    queue.append(curr.left)
-                if curr.right:
-                    next_sum += curr.right.val
-                    curr.right.val = sibling_sum
-                    queue.append(curr.right)
-
-            curr_sum = next_sum
-        
+            next_level_sum = 0
+            curr_level_len = len(queue)
+            for _ in range(curr_level_len):
+                curr_node = queue.popleft()
+                curr_node.val = curr_level_sum - curr_node.val
+                curr_children_sum = (curr_node.left.val if curr_node.left else 0) + \
+                                    (curr_node.right.val if curr_node.right else 0)
+                next_level_sum += curr_children_sum
+                if curr_node.left:
+                    curr_node.left.val = curr_children_sum
+                    queue.append(curr_node.left)
+                if curr_node.right:
+                    curr_node.right.val = curr_children_sum
+                    queue.append(curr_node.right)
+            curr_level_sum = next_level_sum
         return root
 
 class BfsAndHash:
