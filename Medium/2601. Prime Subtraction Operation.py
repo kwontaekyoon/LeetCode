90a53d9d67fe9c9a37be7e294f2cf5d653ded9fc
kwontaekyoon <thomazkwon@gmail.com>
2024-11-11 10:51:00 +0000

    create medium greedy math

---
+++ Medium/2601. Prime Subtraction Operation.py
@@ -0,0 +1,23 @@
+from bisect import bisect_left
+from typing import List
+
+
+class Solution:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        valid = [True] * 1001
+        valid[0] = valid[1] = False
+        for i in range(2, len(valid)):
+            if valid[i]:
+                for j in range(i * i, len(valid), i):
+                    valid[j] = False
+        primes = [i for i in range(len(valid)) if valid[i]]
+
+        prev = 0
+        for num in nums:
+            if num <= prev:
+                return False
+            i = bisect_left(primes, num - prev) - 1
+            if i != -1:
+                num -= primes[i]
+            prev = num
+        return True
