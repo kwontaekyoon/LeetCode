5e80688161eed87befdbd36031985fca9165f3a8
kwontaekyoon <thomazkwon@gmail.com>
2024-12-26 17:05:44 +0000

    create medium dp math

---
+++ Medium/494. Target Sum.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        if (sum(nums) + target) % 2 != 0 or sum(nums) < abs(target):
+            return 0
+        subtarget = (sum(nums) + target)//2
+        dp = [0] * (subtarget+1)
+        dp[0] = 1
+        for num in nums:
+            for i in range(subtarget, num-1, -1):
+                dp[i] += dp[i-num]
+        return dp[subtarget]
