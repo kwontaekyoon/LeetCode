af978f443d5736a0bacc18b4ab0d08e1dfb45b49
kwontaekyoon <thomazkwon@gmail.com>
2025-03-02 10:56:53 +0100

    update easy two pointers array

--- Easy/2570. Merge Two 2D Arrays by Summing Values.py
+++ Easy/2570. Merge Two 2D Arrays by Summing Values.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class TwoPointers:
+class Solution1:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
         n, m, i, j = len(nums1), len(nums2), 0, 0
         res = []
@@ -16,29 +16,22 @@ class TwoPointers:
             else:
                 res.append(nums2[j])
                 j += 1
-        while i < n:
-            res.append(nums1[i])
-            i += 1
-        while j < m:
-            res.append(nums2[j])
-            j += 1
-        return res
+        return res + nums1[i:] + nums2[j:]
 
-class TwoPointers1:
+class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
         m = len(nums2)
         j = 0
         res = []
-        for num1 in nums1:
+        for i, num1 in enumerate(nums1):
             while j < m and nums2[j][0] < num1[0]:
                 res.append(nums2[j])
                 j += 1
-            if j < m and nums2[j][0] == num1[0]:
+            if j == m:
+                return res + nums1[i:]
+            if nums2[j][0] == num1[0]:
                 res.append([num1[0], num1[1] + nums2[j][1]])
                 j += 1
             else:
                 res.append(num1)
-        while j < m:
-            res.append(nums2[j])
-            j += 1
-        return res
+        return res + nums2[j:]
