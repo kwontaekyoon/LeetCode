d8fc0f055b4bc3778f34eda6eca6899dff3e0df6
kwontaekyoon <thomazkwon@gmail.com>
2025-04-19 10:32:24 +0100

    update medium sorting binary search

--- Medium/2563. Count the Number of Fair Pairs.py
+++ Medium/2563. Count the Number of Fair Pairs.py
@@ -11,3 +11,17 @@ class Solution:
             upper_end = bisect_right(nums, upper - nums[i], i + 1)
             cnt += upper_end - lower_end
         return cnt
+    
+class Sorting:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        def count(limit: int) -> int:
+            result = 0
+            low, high = 0, len(nums) - 1
+            while low < high:
+                while low < high and nums[low] + nums[high] > limit:
+                    high -= 1
+                result += high - low
+                low += 1
+            return result
+        nums.sort()
+        return count(upper) - count(lower - 1)
