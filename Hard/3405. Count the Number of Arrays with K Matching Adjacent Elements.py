cb8d5d77171b40793d0a9ea46639a6be5e39e794
kwontaekyoon <thomazkwon@gmail.com>
2025-06-17 09:38:36 +0100

    create hard combinatorics math

---
+++ Hard/3405. Count the Number of Arrays with K Matching Adjacent Elements.py
@@ -0,0 +1,19 @@
+class Solution1:
+    def countGoodArrays(self, n: int, m: int, k: int) -> int:
+        def comb(a, b):
+            return fac[a] * inv[b] % mod * inv[a - b] % mod if 0 <= b <= a else 0
+
+        mod = 10 ** 9 + 7
+        if k > n - 1:
+            return 0
+
+        fac = [1] * n
+        for i in range(1, n):
+            fac[i] = fac[i - 1] * i % mod
+
+        inv = [1] * n
+        inv[n - 1] = pow(fac[n - 1], mod - 2, mod)
+        for i in range(n - 1, 0, -1):
+            inv[i - 1] = inv[i] * i % mod
+
+        return m * comb(n - 1, k) % mod * pow(m - 1, n - 1 - k, mod) % mod      
