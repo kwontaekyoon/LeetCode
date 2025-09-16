7d55454a8342c5d9488686f44faf49d32cb7fcaf
kwontaekyoon <thomazkwon@gmail.com>
2025-09-16 10:05:44 +0100

    create hard math stack

---
+++ Hard/2197. Replace Non-Coprime Numbers in Array.py
@@ -0,0 +1,12 @@
+import math
+from typing import List
+
+
+class Solution:
+    def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
+        stack = []
+        for num in nums:
+            while stack and math.gcd(stack[-1], num) > 1:
+                num = math.lcm(num, stack.pop())
+            stack.append(num)
+        return stack
