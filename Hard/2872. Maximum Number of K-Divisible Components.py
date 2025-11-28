a3873c3606ec8cfc05e8f908806bcea68794c7a2
kwontaekyoon <thomazkwon@gmail.com>
2025-11-28 09:54:51 +0000

    update medium dfs

--- Hard/2872. Maximum Number of K-Divisible Components.py
+++ Hard/2872. Maximum Number of K-Divisible Components.py
@@ -2,7 +2,29 @@ from collections import defaultdict, deque
 from typing import List
 
 
-class Solution:
+class DFS:
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        graph = defaultdict(list)
+        # graph = [[] for _ in range(n)]
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        self.result = 0
+
+        def dfs(prv: int, crr: int) -> int:
+            s = values[crr]
+            for nxt in graph[crr]:
+                if nxt != prv:
+                    s += dfs(crr, nxt)
+            if s % k == 0:
+                self.result += 1
+            return s % k
+        
+        dfs(-1, 0)
+        return self.result
+
+class Solution1:
     def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
         if n < 2:
             return 1
