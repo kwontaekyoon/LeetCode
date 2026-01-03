76c32fd1cdf3a11bba3d335fbbcd879b28710913
kwontaekyoon <thomazkwon@gmail.com>
2026-01-03 17:13:39 +0000

    create hard math

---
+++ Hard/1411. Number of Ways to Paint N × 3 Grid.py
@@ -0,0 +1,8 @@
+MOD = 1000000007
+
+class Solution:
+    def numOfWays(self, n: int) -> int:
+        A = B = 6
+        for _ in range(2, n + 1):
+            A, B = (2 * A + 2 * B) % MOD, (2 * A + 3 * B) % MOD
+        return (A + B) % MOD
