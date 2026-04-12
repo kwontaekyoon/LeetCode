7aede000b9b15f0d5b3a5ff5c0590922542a7036
teq <45059797+kwontaekyoon@users.noreply.github.com>
2026-04-12 01:35:10 +0100

    create medium hash table

---
+++ Medium/3741. Minimum Distance Between Three Equal Elements II.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        num_to_idxs = defaultdict(list)
+        for i, v in enumerate(nums):
+            num_to_idxs[v].append(i)
+        min_dist = float('inf')
+        for li in num_to_idxs.values():
+            if len(li) < 3:
+                continue
+            for i in range(1, len(li) - 1):
+                min_dist = min(abs(li[i + 1] - li[i - 1]), min_dist)
+        return -1 if min_dist == float('inf') else min_dist * 2
