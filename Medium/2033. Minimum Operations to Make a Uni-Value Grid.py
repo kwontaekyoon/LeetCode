fb774d9f70494e38be6295156f36cbc7969e1f19
teq <45059797+kwontaekyoon@users.noreply.github.com>
2026-04-28 15:25:15 +0100

    update medium sorting

--- Medium/2033. Minimum Operations to Make a Uni-Value Grid.py
+++ Medium/2033. Minimum Operations to Make a Uni-Value Grid.py
@@ -3,6 +3,20 @@ from typing import List
 
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
+        nums = [num for row in grid for num in row]
+        rem = nums[0] % x
+        for num in nums:
+            if num % x != rem:
+                return -1
+        nums.sort()
+        median = nums[len(nums) // 2]
+        ops = 0
+        for num in nums:
+            ops += abs(num - median) // x
+        return ops
+
+class Solution1:
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
         nums = sorted([num for row in grid for num in row])
         for idx, num in enumerate(nums):
             if (num - nums[idx - 1]) % x:
