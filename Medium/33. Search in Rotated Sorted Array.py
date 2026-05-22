d5f19776f6bd6dc7bd94ab8e86abc49c4c2d0e80
teq <45059797+kwontaekyoon@users.noreply.github.com>
2026-05-22 09:28:26 +0100

    create medium binary search

---
+++ Medium/33. Search in Rotated Sorted Array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] > nums[r]:
+                if nums[m] > target and nums[l] <= target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                if nums[m] < target and nums[r] >= target:
+                    l = m + 1
+                else:
+                    r = m - 1
+        return -1
